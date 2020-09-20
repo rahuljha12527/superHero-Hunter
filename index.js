48151cf8829f9066b410360f3bdc0d196c89d747
rahuljha661 <rahuljha12527@gmail.com>
2020-09-20 22:46:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const url = "https://www.superheroapi.com/api.php/1644433292405040";
+const searchBox=document.querySelector("#search");
+
+const searchResultContainer=document.getElementById('search-results-container');
+
+//key change
+
+loadEventListner();
+
+
+
+
